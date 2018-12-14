129b5ee0580d9989bb5d6b49ff09374cb6581d34
李伟 <liwei@9ffenqigo.com>
2018-12-14 10:37:37 +0800

    修改内容

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  title: "李伟",
+  description: "The description of the site.",
+  head: [["link", { rel: "icon", href: `/logo.png` }]],
+  base: "/",
+  dest: "./dist",
+
+  themeConfig: {
+    nav: [
+      { text: "主页", link: "/" },
+      { text: "项目", link: "/projects/" },
+      { text: "博客", link: "/guide/" },
+      { text: "GitHub", link: "https://github.com/akuraSnow" }
+    ],
+    sidebar: {
+      '/guide/': genSidebarConfig('Guide')
+    },
+    lastUpdated: 'Last Updated'
+  },
+
+  markdown: {
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+    config: md => {
+      md.use(require("markdown-it-katex"));
+    }
+  }
+};
+
+function genSidebarConfig (title) {
+  return [
+    {
+      title,
+      collapsable: false,
+      children: [
+        '',
+        'getting-started',
+        'customize',
+        'advanced',
+      ]
+    }
+  ]
+}
+
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,22 @@
+---
+layout: Homepage
+description: 'The biography and information about me.'
+avatar: /profile.jpg
+head: '李伟'
+info: '成都信息工程大学银杏酒店管理学院'
+interests: '兴趣: 篮球 游戏 旅游.'
+socials:
+- title: github
+  link: https://github.com/akuraSnow
+
+actions:
+- text: 项目
+  link: /projects/
+- text: 博客
+  link: https://github.com/mtobeiyf
+- text: 文章
+  link: /article/
+footer: Made with ♥ by Fing. Powered by VuePress
+---
+
 
