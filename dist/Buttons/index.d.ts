c29154f75e4ef906eeea2b4cc6d6ac4801bb453a
liwei <15196677082@163.com>
2020-08-17 16:09:59 +0800

    Updates

--- dist/Buttons/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react" />
-import './index.less';
-declare const Button: (props: any) => JSX.Element;
-export default Button;
--- dist/uploadFile/servers/request.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * 配置request请求时的默认参数
- */
-declare const request: import("umi-request").RequestMethod<false>;
-export default request;
